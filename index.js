97c4b6aaa72eec2a89e81da3684899a5fa5f5615
Prakhar Pande <pandeprakhar1801@gmail.com>
2024-09-30 10:25:16 +0530

    mongodb functionality

--- index.js
+++ index.js
@@ -1,41 +1,83 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://pandeprakhar1801:cEtdEgYSzuzKAYDy@ploggify.0wx5z.mongodb.net/?retryWrites=true&w=majority&appName=ploggify')
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
 const userSchema = new mongoose.Schema({
-    name : String,
-    age : Number
+    username : String,
+    useremail : String,
+    userpassword : String,
+    userphno : String,
+    userage : String,
+    usergender : String,
+    userexperience : String
 });
 
 const userModel = mongoose.model("userModel", userSchema);
 
-app.post("/api/data", (req,res) => {
-    res.json(userModel.find().then(function(users){
-        res.json(users);
-        console.log(users);
-    })).catch(function(err){
-        console.log('error');
-    });
-})
+app.post('/api/data', async (req, res) => {
+    try {
+      const receivedData = req.body.data;
+      console.log('Received data:', receivedData);
 
-const newUser = new userModel({
-    name : "Prakhar",
-    age : 49
-});
+      putData(receivedData);
+    }
+    catch(error){
+        console.log("data received");
+    }});
 
-userModel.insertMany(newUser)
-  .then(user => console.log('User saved:', user))
-  .catch(err => console.error('Error saving user:', err));
+function putData(receivedData){
+    const newUser = new userModel(receivedData);
+
+    userModel.insertMany(newUser)
+    .then(user => console.log('User saved:', user))
+    .catch(err => console.error('Error saving user:', err));
+}
+
+function getData(){
+    userModel.find()
+    .then(users => console.log('All users:', users))
+    .catch(err => console.error('Error retrieving users:', err));
+}
 
 
-userModel.find()
-.then(users => console.log('All users:', users))
-.catch(err => console.error('Error retrieving users:', err));
+
+// app.post("/api/data", (req,res) => {
+//     res.json(userModel.find().then(function(users){
+//         res.json(users);
+//         console.log(users);
+//     })).catch(function(err){
+//         console.log('error');
+//     });
+// })
+
+// const newUser = new userModel({
+//     username : ,
+//     useremail : ,
+//     userpassword : ,
+//     userphno : ,
+//     userage : ,
+//     usergender : ,
+//     userexperience : 
+// });
+
+// userModel.insertMany(newUser)
+//   .then(user => console.log('User saved:', user))
+//   .catch(err => console.error('Error saving user:', err));
+
+
+// userModel.find()
+// .then(users => console.log('All users:', users))
+// .catch(err => console.error('Error retrieving users:', err));
 
 app.listen(5000, () => {
     console.log('server listening at 5000');
